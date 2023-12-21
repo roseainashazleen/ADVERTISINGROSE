fd854b0dd19f05598216e8636b7eb630f616e507
roseainashazleen <154338276+roseainashazleen@users.noreply.github.com>
2023-12-21 15:05:06 +0800

    Advertising-generate-predict.py

--- Advertising-generate-predict.py
+++ Advertising-generate-predict.py
@@ -11,7 +11,7 @@ st.sidebar.header('User Input Parameters')
 def user_input_features():
     TV = st.sidebar.slider('TV', 0.70, 296.4, 296.4)
     Radio = st.sidebar.slider('Radio', 0.0, 296.4, 30.0)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 200.0, 200.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 50.0, 114.0)
 
     data = {'TV': TV,
             'Radio': Radio,
